b456ce2f2ba9c91b8ec5fcb535d285192e0548ed
NikFel1995 <stena-55@mail.ru>
2016-05-18 12:49:45 +0500

    Добавил свойства в класс Point

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -4,15 +4,33 @@ namespace Snake
 {
     internal class Point
     {
-        private readonly int _x;
-        private readonly int _y;
-        private readonly char _sym;
+        private int _x;
+        private int _y;
+        private char _sym;
 
         public Point(int x, int y, char sym)
         {
             _x = x;
             _y = y;
             _sym = sym;
+        }
+
+        public int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public int Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public char Sym
+        {
+            get { return _sym; }
+            set { _sym = value; }
         }
 
         public void Draw()
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -13,7 +13,8 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            new Point(10,6,'P').Draw();
+
+
 
             Console.ReadKey();
         }
