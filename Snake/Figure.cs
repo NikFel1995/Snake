e1a5b4d9dd946b6bae28edeabd4e6009050bee4f
NikFel1995 <stena-55@mail.ru>
2016-05-18 14:12:33 +0500

    Добавлен родительский класс Figure

---
+++ Snake/Figure.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snake
+{
+    internal class Figure
+    {
+        /// <summary>
+        /// Список точек
+        /// </summary>
+        protected List<Point> PointList;
+
+        /// <summary>
+        /// Вывод вертикальной линии на экран
+        /// </summary>
+        public void Draw()
+        {
+            foreach (var point in PointList)
+            {
+                point.Draw();
+            }
+        }
+
+        /// <summary>
+        /// Вывод вертикальной линии на экран
+        /// <param name="color">Цвет линии</param>
+        /// </summary>
+        public void Draw(ConsoleColor color)
+        {
+            foreach (var point in PointList)
+            {
+                point.Draw(color);
+            }
+        }
+    }
+}
