94454355924e43d3b07710b839006717d0bb38c2
NikFel1995 <stena-55@mail.ru>
2016-05-18 14:40:55 +0500

    Добавлен класс вертикальные линии

---
+++ Snake/VerticalLine.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections.Generic;
+
+namespace Snake
+{
+    class VerticalLine : Figure
+    {
+
+        public VerticalLine(int yUp, int yDown, int x, char sym)
+        {
+            PointList = new List<Point>();
+            if (yUp > yDown)
+                HelperMethods.Swap(ref yUp, ref yDown);
+            for (int y = yUp; y <= yDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                PointList.Add(p);
+            }
+        }
+    }
+}
