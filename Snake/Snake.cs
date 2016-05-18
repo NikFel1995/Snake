e8d11a35441f6dccfd6e8070852a5ea630b9e283
NikFel1995 <stena-55@mail.ru>
2016-05-18 15:11:56 +0500

    Добавлен класс Snake

---
+++ Snake/Snake.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections.Generic;
+
+namespace Snake
+{
+    internal class Snake : Figure
+    {
+
+        public Snake(Point tail, int length, Direction direction)
+        {
+            PointList = new List<Point>();
+
+            for (int i = 0; i < length; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                PointList.Add(p);
+            }
+        }
+    }
+}
