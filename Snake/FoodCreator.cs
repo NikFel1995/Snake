7ab1604b2128c4d2f6a3e9764dd6470284895694
NikFel1995 <stena-55@mail.ru>
2016-05-19 14:15:59 +0500

    Создан класс для генерации еды

---
+++ Snake/FoodCreator.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace Snake
+{
+    class FoodCreator
+    {
+        private readonly int _mapWidth;
+        private readonly int _mapHeight;
+        private readonly char _sym;
+
+        private readonly Random _random = new Random();
+
+        public FoodCreator(int mapWidth, int mapHeight, char sym)
+        {
+            _mapWidth = mapWidth;
+            _mapHeight = mapHeight;
+            _sym = sym;
+        }
+
+        public Point CreateFood()
+        {
+            int x = _random.Next(2, _mapWidth - 2);
+            int y = _random.Next(2, _mapHeight - 2);
+
+            return new Point(x, y, _sym);
+        }
+
+    }
+}
